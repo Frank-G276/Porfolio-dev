01840bb6b3d90f2e4e149af2f6623f7d9e74333d
Frank_G <guilombofrank07@gmail.com>
2024-12-26 11:52:58 -0500

    Se crea la estrutra basica

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,,ts,tsx,}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#ffffff', // Example primary color
+				secondary: '#9333EA', // Example secondary color
+			},
+		},
+	},
+	plugins: [],
+}
